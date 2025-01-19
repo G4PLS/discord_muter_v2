7eb9412a777b77af49e56bb10753c556ce9af9e3
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-19 20:57:06 +0100

    Moved ConVars to discord_muter

--- lua/terrortown/utils/logging.lua
+++ lua/terrortown/utils/logging.lua
@@ -1,7 +1,5 @@
 globals = include("terrortown/autorun/globals.lua")
 
-CreateConVar(globals.con_vars.DEBUG, 0, FCVAR_UNREGISTERED, "Print debug messages to the Console")
-
 function log_debug(...)
     local isDebuggingEnabled = GetConVar(globals.con_vars.DEBUG):GetBool()
     local currentTime = os.date("%Y-%m-%d %H:%M:%S")
