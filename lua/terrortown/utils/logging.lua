8e256b45c54532bfbdbc154c3c2c5125be43a55b
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-19 20:27:01 +0100

    Refactor(ConVars): Changed to "standard" naming convention

--- lua/terrortown/utils/logging.lua
+++ lua/terrortown/utils/logging.lua
@@ -1,4 +1,4 @@
-CreateConVar("DiscordDebug", 0, FCVAR_UNREGISTERED, "Print debug messages to the Console")
+CreateConVar("discord_debug", 0, FCVAR_UNREGISTERED, "Print debug messages to the Console")
 
 function log_debug(...)
     local isDebuggingEnabled = GetConVar("DiscordDebug"):GetBool()
