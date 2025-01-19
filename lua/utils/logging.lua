6016c885ccb4f2587bbc97f279b13e3adf413199
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-19 22:33:48 +0100

    Feat(Logging): Adedd better functions to logging and made globals local

--- lua/utils/logging.lua
+++ lua/utils/logging.lua
@@ -1,11 +1,27 @@
-globals = include("globals.lua")
+local globals = include("globals.lua")
 
-function log_debug(...)
+function getLogStatus()
     local isDebuggingEnabled = GetConVar(globals.con_vars.DEBUG):GetBool()
 
+    if isDebuggingEnabled == nil then
+        isDebuggingEnabled = false
+    end
+
+    return isDebuggingEnabled
+end
+
+function logDebug(...)
     local currentTime = os.date("%Y-%m-%d %H:%M:%S")
 
-    if isDebuggingEnabled then
-        print(("%s [discord_muter_v2]:"):format(currentTime),...)
+    if getLogStatus() then
+        print(("%s [discord_muter_v2 DEBUG]:"):format(currentTime),...)
+    end
+end
+
+function logError(...)
+    local currentTime = os.date("%Y-%m-%d %H:%M:%S")
+
+    if getLogStatus() then
+        print(("%s [discord_muter_v2 ERROR]:"):format(currentTime),...)
     end
 end
