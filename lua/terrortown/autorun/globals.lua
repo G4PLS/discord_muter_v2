2c4844bc98bafa5e06c8d7bf1dee83b7315db3fc
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-19 20:38:10 +0100

    Feat(globals): Added globals file that stores all relevant names

---
+++ lua/terrortown/autorun/globals.lua
@@ -0,0 +1,26 @@
+globals = {
+    con_vars = {
+        DEBUG = "discord_muter_debug",
+        ENDPOINT = "discord_muter_endpoint",
+        API_KEY = "discord_muter_api_key",
+        DISCORD_NAME = "discord_muter_name",
+        SERVER_LINK = "discord_muter_server_link",
+        MUTE_ROUND = "discord_muter_muter_round",
+        MUTE_DURATION = "discord_muter_mute_duration",
+        AUTO_CONNECT = "discord_muter_auto_connect"
+    },
+    network_strings = {
+        DRAW_MUTE = "drawMute",
+        CONNECT_DISCORD_ID = "connectDiscordID",
+        PLAYER_TABLE = "discordPlayerTable",
+        REQUEST_PLAYER_TABLE = "request_discordPlayerTable",
+        TEST_CONNECTION = "discordTestConnection",
+        REQUEST_TEST_CONNECTION = "request_discordTestConnection",
+        ADDON_VERSION = "addonVersion",
+        REQUEST_ADDON_VERSION = "request_addonVersion",
+        BOT_VERSION = "botVersion",
+        REQUEST_BOT_VERSION = "request_botVersion"
+    }
+}
+
+return globals
--- lua/terrortown/utils/logging.lua
+++ lua/terrortown/utils/logging.lua
@@ -1,7 +1,9 @@
-CreateConVar("discord_debug", 0, FCVAR_UNREGISTERED, "Print debug messages to the Console")
+globals = include("terrortown/autorun/globals.lua")
+
+CreateConVar(globals.con_vars.DEBUG, 0, FCVAR_UNREGISTERED, "Print debug messages to the Console")
 
 function log_debug(...)
-    local isDebuggingEnabled = GetConVar("DiscordDebug"):GetBool()
+    local isDebuggingEnabled = GetConVar(globals.con_vars.DEBUG):GetBool()
     local currentTime = os.date("%Y-%m-%d %H:%M:%S")
 
     if isDebuggingEnabled then
