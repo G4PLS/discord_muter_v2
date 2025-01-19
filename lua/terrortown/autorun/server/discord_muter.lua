79fb155a559e3605c243258935e633ddf6240078
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-19 21:04:58 +0100

    Loading ConVars before calling logging methods

--- lua/terrortown/autorun/server/discord_muter.lua
+++ lua/terrortown/autorun/server/discord_muter.lua
@@ -1,9 +1,4 @@
-include("terrortown/hooks/discord_hooks.lua")
-include("terrortown/utils/logging.lua")
-
 globals = include("terrortown/autorun/globals.lua")
-
-log_debug("AUTORUN SERVER V1")
 
 util.AddNetworkString(globals.network_strings.DRAW_MUTE)
 util.AddNetworkString(globals.network_strings.CONNECT_DISCORD_ID)
@@ -16,9 +11,6 @@ util.AddNetworkString(globals.network_strings.REQUEST_ADDON_VERSION)
 util.AddNetworkString(globals.network_strings.BOT_VERSION)
 util.AddNetworkString(globals.network_strings.REQUEST_BOT_VERSION)
 
-print("DISCORD")
-print(globals.con_vars.ENDPOINT)
-
 CreateConVar(globals.con_vars.DEBUG, 0, FCVAR_UNREGISTERED, "Print debug messages to the Console")
 CreateConVar(globals.con_vars.ENDPOINT, "http://localhost:37405", 1, "Sets the node bot endpoint.")
 CreateConVar(globals.con_vars.API_KEY, "", 1, "Sets the node bot api-key.")
@@ -27,3 +19,8 @@ CreateConVar(globals.con_vars.SERVER_LINK, "https://discord.gg/", 1, "Sets the D
 CreateConVar(globals.con_vars.MUTE_ROUND, 1, 1, "Mute the player until the end of the round.", 0, 1)
 CreateConVar(globals.con_vars.MUTE_DURATION, 5, 1, "Sets how long, in seconds, you are muted for after death. No effect if mute_round is on. ", 1, 60)
 CreateConVar(globals.con_vars.AUTO_CONNECT, 0, 1, "Attempt to automatically match player name to discord name. This happens silently when the player connects. If it fails, it will prompt the user with the \"!discord NAME\" message.", 0, 1)
+
+include("terrortown/utils/logging.lua")
+include("terrortown/hooks/discord_hooks.lua")
+
+log_debug("AUTORUN SERVER V1")
--- lua/terrortown/utils/logging.lua
+++ lua/terrortown/utils/logging.lua
@@ -1,5 +1,10 @@
 globals = include("terrortown/autorun/globals.lua")
 
 function log_debug(...)
-    return
+    local isDebuggingEnabled = GetConVar(globals.con_vars.DEBUG):GetBool()
+    local currentTime = os.date("%Y-%m-%d %H:%M:%S")
+
+    if isDebuggingEnabled then
+        print(("%s [discord_muter_v2]: "):format(currentTime), ...)
+    end
 end
