adcb730175ac1aeff5339842c2d4f183d6c2de2d
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-19 21:00:28 +0100

    Removed everything from log_debug

--- lua/terrortown/utils/logging.lua
+++ lua/terrortown/utils/logging.lua
@@ -1,10 +1,5 @@
 globals = include("terrortown/autorun/globals.lua")
 
 function log_debug(...)
-    local isDebuggingEnabled = GetConVar(globals.con_vars.DEBUG):GetBool()
-    local currentTime = os.date("%Y-%m-%d %H:%M:%S")
-
-    if isDebuggingEnabled then
-        print(("%s [discord_muter_v2]: "):format(currentTime), ...)
-    end
+    return
 end
