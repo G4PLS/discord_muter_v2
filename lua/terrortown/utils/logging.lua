193ff7763edc523684802c699e2a34fb5f96f998
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-19 20:10:24 +0100

    Feat(Logging): Added current time to logging method

--- lua/terrortown/utils/logging.lua
+++ lua/terrortown/utils/logging.lua
@@ -2,7 +2,9 @@ CreateConVar("DiscordDebug", 0, FCVAR_UNREGISTERED, "Print debug messages to the
 
 function log_debug(...)
     local isDebuggingEnabled = GetConVar("DiscordDebug"):GetBool()
+    local currentTime = os.date("%Y-%m-%d %H:%M:%S")
+
     if isDebuggingEnabled then
-        print("[Discord Muter]: ", ...)
+        print(("%s [discord_muter_v2]: "):format(currentTime), ...)
     end
 end
