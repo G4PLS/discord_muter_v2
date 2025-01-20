f35e95b8cab709dd496a672a8a20cece3fb9490b
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-20 13:29:43 +0100

    Writing bit instead of bool

--- lua/autorun/client/cl_muter.init.lua
+++ lua/autorun/client/cl_muter.init.lua
@@ -4,12 +4,12 @@ local globals = include("globals.lua")
 concommand.Add(globals.con_vars.DEBUG,
 function(ply, cmd, args, argStr)
     net.Start(globals.network.server.DEBUG)
-    net.WriteBool(args[1])
+    net.WriteBit(args[0])
     net.SendToServer()
 end,
 function(cmd, argStr, args)
     return {
-        globals.con_vars.DEBUG .. ' ' .. '0',
-        globals.con_vars.DEBUG .. ' ' .. '1'
+        globals.con_vars.DEBUG .. ' ' .. "1",
+        globals.con_vars.DEBUG .. ' ' .. "0"
     }
 end)
