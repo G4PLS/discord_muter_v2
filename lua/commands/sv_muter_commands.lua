653b62a33adfe15734c3eba9d663af21e2f574f1
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-20 19:23:41 +0100

    Cache file printing

--- lua/commands/sv_muter_commands.lua
+++ lua/commands/sv_muter_commands.lua
@@ -1,5 +1,14 @@
 local logger = include("utils/logger.lua")
+include("discord_integration/sv_id_mapper.lua")
 
 concommand.Add("muter_list_ids", function(ply, cmd, args, argStr)
     logger.logTable(_G.id_mapping, "Connection Mapping", "Listing connection mappings")
+end)
+
+concommand.Add("muter_print_cache_file", function(ply, cmd, args, argStr)
+    logger.logDebug("Printing cache file")
+    local id_mapping_cache = file.Read(ID_MAPPING_CACHE_PATH .. ".json", "DATA")
+    logger.logDebug(id_mapping_cache)
+    local json_id_mapping_cache = util.JSONToTable(id_mapping_cache)
+    logger.logTable(json_id_mapping_cache, "JSON Cache", "Printing Json Cache")
 end)
