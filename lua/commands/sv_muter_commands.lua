4896e1c403c48b0ddac5577714078af73c629e35
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-20 19:24:47 +0100

    force cache loading

--- lua/commands/sv_muter_commands.lua
+++ lua/commands/sv_muter_commands.lua
@@ -7,7 +7,7 @@ end)
 
 concommand.Add("muter_print_cache_file", function(ply, cmd, args, argStr)
     logger.logDebug("Printing cache file")
-    local id_mapping_cache = file.Read(ID_MAPPING_CACHE_PATH .. ".json", "DATA")
+    local id_mapping_cache = file.Read("discord_connection_cache" .. ".json", "DATA")
     logger.logDebug(id_mapping_cache)
     local json_id_mapping_cache = util.JSONToTable(id_mapping_cache)
     logger.logTable(json_id_mapping_cache, "JSON Cache", "Printing Json Cache")
