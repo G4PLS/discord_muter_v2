0f4426b31f3da2a0be338f53a9f57982dcaaf6ef
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-21 18:03:04 +0100

    added simple categroy?

---
+++ lua/ulx/modules/sh_muter.lua
@@ -0,0 +1,8 @@
+local CATEGORY_NAME = "discord"
+
+function ulx.mute(calling_ply, target_ply, duration, should_unmute)
+    print(calling_ply:Nick())
+    print(target_ply:Nick())
+    print(duration)
+    print(should_unmute)
+end
