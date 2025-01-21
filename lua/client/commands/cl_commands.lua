83f1873e37bad4ed7ae7384db9612d9dba96182c
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-21 11:47:16 +0100

    Added more commands

--- lua/client/commands/cl_commands.lua
+++ lua/client/commands/cl_commands.lua
@@ -1,4 +1,108 @@
-include("muter_globals/sh_globals.lua")
+include("shared/globals/sh_globals.lua")
+include("client/globals/cl_globals.lua")
+
+concommand.Add(commands.SET_DEBUG_ENABLED, function(ply, cmd, args, argStr)
+    local debug_enabled = args[1] == "1"
+
+    net.Start(network.server.SET_DEBUG)
+    net.WriteBit(debug_enabled)
+    net.SendToServer()
+end, function(cmd, args)
+    return {
+        commands.SET_DEBUG_ENABLED .. ' ' .. '0',
+        commands.SET_DEBUG_ENABLED .. ' ' .. '1'
+    }
+end)
+
+concommand.Add(commands.SET_DEBUG_LOG_TIME_ENABLED, function(ply, cmd, args, argStr)
+    local debug_time_enabled = args[1] == "1"
+
+    net.Start(network.server.SET_LOG_TIME)
+    net.WriteBit(debug_time_enabled)
+    net.SendToServer()
+end, function(cmd, args)
+    return {
+        commands.SET_DEBUG_LOG_TIME_ENABLED .. ' ' .. '0',
+        commands.SET_DEBUG_LOG_TIME_ENABLED .. ' ' .. '1'
+    }
+end)
+
+concommand.Add(commands.SET_DEBUG_LOG_LEVELS, function(ply, cmd, args, argStr)
+    local log_levels = ""
+
+    for i=1, #args do
+        log_levels = args[i] .. "|"
+    end
+
+    net.Start(network.server.SET_LOG_LEVELS)
+    net.WriteString(log_levels)
+    net.SendToServer()
+end, function(cmd, args)
+    local auto_completed = {}
+
+    for i=1, #LOG_LEVELS do
+        table.insert(auto_completed, commands.SET_DEBUG_LOG_LEVELS .. ' ' .. LOG_LEVELS[i])
+    end
+
+    return auto_completed
+end)
+
+concommand.Add(commands.SET_BOT_ENDPOINT, function(ply, cmd, args, argStr)
+    net.Start(network.server.SET_BOT_ENDPOINT)
+    net.WriteString(args[1])
+    net.SendToServer()
+end)
+
+concommand.Add(commands.SET_BOT_API_KEY, function(ply, cmd, args, argStr)
+    net.Start(network.server.SET_BOT_API_KEY)
+    net.WriteString(args[1])
+    net.SendToServer()
+end)
+
+concommand.Add(commands.SET_SERVER_LINK, function(ply, cmd, args, argStr)
+    net.Start(network.server.SET_SERVER_LINK)
+    net.WriteString(args[1])
+    net.SendToServer()
+end)
+
+concommand.Add(commands.SET_MUTER_ENABLED, function(ply, cmd, args, argStr)
+    local muter_enabled = args[1] == "1"
+
+    net.Start(network.server.SET_MUTER_ENABLED)
+    net.WriteBit(muter_enabled)
+    net.SendToServer()
+end, function(cmd, args)
+    return {
+        commands.SET_MUTER_ENABLED .. ' ' .. '0',
+        commands.SET_MUTER_ENABLED .. ' ' .. '1'
+    }
+end)
+
+concommand.Add(commands.SET_MUTE_DURATION, function(ply, cmd, args, argStr)
+    local mute_duration = tonumber(args[1])
+
+    if mute_duration == nil then
+        return
+    end
+
+    net.Start(network.server.SET_MUTE_DURATION)
+    net.WriteInt(mute_duration)
+    net.SendToServer()
+end)
+
+concommand.Add(commands.SET_AUTO_CONNECT_ENABLED, function(ply, cmd, args, argStr)
+    local auto_connect_enabled = args[1] == "1"
+
+    net.Start(network.server.SET_AUTO_CONNECT)
+    net.WriteBit(auto_connect_enabled)
+    net.SendToServer()
+end, function(cmd, args)
+    return {
+        commands.SET_AUTO_CONNECT_ENABLED .. ' ' .. '0',
+        commands.SET_AUTO_CONNECT_ENABLED .. ' ' .. '1'
+    }
+end)
+
 
 concommand.Add("muter_add_id", function(ply, cmd, args, argStr)
     local user_id = args[1]
@@ -15,17 +119,5 @@ end)
 
 concommand.Add("muter_clear_ids", function(ply, cmd, args, argStr)
     net.Start(network.server.CLEAR_USER_DISCORD_ID)
-    net.SendToServer()
-end)
-
-concommand.Add("muter_set_bot_endpoint", function(ply ,cmd, args, argStr)
-    net.Start(network.server.BOT_ENDPOINT)
-    net.WriteString(args[1])
-    net.SendToServer()
-end)
-
-concommand.Add("muter_set_bot_api_key", function(ply, cmd, args, argStr)
-    net.Start(network.server.BOT_API_KEY)
-    net.WriteString(args[1])
     net.SendToServer()
 end)
---
+++ lua/client/globals/cl_globals.lua
@@ -0,0 +1,19 @@
+commands = {
+    -- Con Vars
+    SET_DEBUG_ENABLED = "dm_debug_enabled",
+    SET_DEBUG_LOG_TIME_ENABLED = "dm_log_time_enabled",
+    SET_DEBUG_LOG_LEVELS = "dm_log_levels",
+    SET_BOT_ENDPOINT = "dm_bot_endpoint",
+    SET_BOT_API_KEY = "dm_bot_api_key",
+    SET_SERVER_LINK = "dm_server_link",
+    SET_MUTER_ENABLED = "dm_enabled",
+    SET_MUTE_DURATION = "dm_mute_duration",
+    SET_AUTO_CONNECT_ENABLED = "dm_auto_connect",
+
+    -- Other
+    ADD_ID = "dm_add_id",
+    REMOVE_ID = "dm_remove_id",
+    CLEAR_IDS = "dm_clear_ids",
+}
+
+LOG_LEVELS = {"INFO", "WARNING", "DEBUG", "ERROR"}
