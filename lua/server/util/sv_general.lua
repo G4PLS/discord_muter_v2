f1e56adb3e0cd2f12728a52a48bfdbe5646bdeaa
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-21 13:53:19 +0100

    A

--- lua/server/util/sv_general.lua
+++ lua/server/util/sv_general.lua
@@ -1,4 +1,5 @@
 include("shared/globals/sh_globals.lua")
+include("shared/logging/sh_logger.lua")
 
 -- 1=Waiting | 2=Prep | 3=Active | 4=Post
 function getRoundState()
@@ -10,10 +11,11 @@ function getRoundState()
 end
 
 function isRoundRunning()
+    logDebug("ROUND RUNNING:")
+    logDebug(tostring(getRoundState()))
     return getRoundState() == 3
 end
 
 function canMute()
-    print(GetConVar(con_vars.ENABLE_MUTER):GetBool())
     return GetConVar(con_vars.ENABLE_MUTER):GetBool() or false
 end
