895f12599110dc125a9edd63388231626f33bc49
gapls <125091918+G4PLS@users.noreply.github.com>
2025-01-21 12:17:55 +0100

    even more commands

--- lua/server/network/sv_network.lua
+++ lua/server/network/sv_network.lua
@@ -12,44 +12,83 @@ function isSuperAdmin(ply)
 end
 
 -- Receiving from Client
-net.Receive(network.server.ADD_USER_DISCORD_ID, function(len, ply)
-    logDebug("Trying to add users discord ID to Mapping")
-
-    local discord_id = net.ReadString()
-    addConnectionID(ply, discord_id)
-end)
-
-net.Receive(network.server.REMOVE_USER_DISCORD_ID,function(len, ply)
-    logDebug("Trying to remove user from discord ID mapping")
+net.Receive(network.server.SET_DEBUG, function(len, ply)
+    logInfo("Trying to set " .. con_vars.DEBUG)
 
     if isSuperAdmin(ply) then
-        logDebug("Removing player " .. ply:Nick() " from mapping")
-        removeConnectionID(ply)
+        local debug_enabled = net.ReadBit()
+        RunConsoleCommand(con_vars.DEBUG, debug_enabled)
     end
 end)
 
-net.Receive(network.server.CLEAR_USER_DISCORD_ID, function(len, ply)
-    logDebug("Trying to clear discord id mappings")
+net.Receive(network.server.SET_LOG_TIME, function(len, ply)
+    logInfo("Trying to set " .. con_vars.LOG_TIME)
+
     if isSuperAdmin(ply) then
-        clearConnectionIDs()
+        local time_logging_enabled = net.ReadBit()
+        RunConsoleCommand(con_vars.LOG_TIME, time_logging_enabled)
     end
 end)
 
-net.Receive(network.server.BOT_ENDPOINT, function(len, ply)
-    logDebug("Trying to set Bot endpoint")
+net.Receive(network.server.SET_LOG_LEVELS, function(len, ply)
+    logInfo("Trying to set " .. con_vars.LOG_LEVELS)
+
     if isSuperAdmin(ply) then
-        local endpoint = net.ReadString()
-        RunConsoleCommand(con_vars.BOT_ENDPOINT, endpoint)
-        logInfo("Bot Endpoint set to: " .. endpoint)
+        local log_levels = net.ReadString()
+        RunConsoleCommand(con_vars.LOG_LEVELS, log_levels)
     end
 end)
 
-net.Receive(network.server.BOT_API_KEY, function(len, ply)
-    logDebug("Trying to set Bot api key")
+net.Receive(network.server.SET_BOT_ENDPOINT, function(len, ply)
+    logInfo("Trying to set " .. con_vars.BOT_ENDPOINT)
 
     if isSuperAdmin(ply) then
-        local api_key = net.ReadString()
-        RunConsoleCommand(con_vars.BOT_API_KEY, api_key)
-        logInfo("Bot api key set to: " .. api_key)
+        local bot_endpoint = net.ReadString()
+        RunConsoleCommand(con_vars.BOT_ENDPOINT, bot_endpoint)
+    end
+end)
+
+net.Receive(network.server.SET_BOT_API_KEY, function(len, ply)
+    logInfo("Trying to set " .. con_vars.BOT_API_KEY)
+
+    if isSuperAdmin(ply) then
+        local bot_api_key = net.ReadString()
+        RunConsoleCommand(con_vars.BOT_API_KEY, bot_api_key)
+    end
+end)
+
+net.Receive(network.server.SET_SERVER_LINK, function(len, ply)
+    logInfo("Trying to set " .. con_vars.SERVER_LINK)
+
+    if isSuperAdmin(ply) then
+        local server_link = net.ReadString()
+        RunConsoleCommand(con_vars.SERVER_LINK, server_link)
+    end
+end)
+
+net.Receive(network.server.SET_MUTER_ENABLED, function(len, ply)
+    logInfo("Trying to set " .. con_vars.ENABLE_MUTER)
+
+    if isSuperAdmin(ply) then
+        local muter_enabled = net.ReadBit()
+        RunConsoleCommand(con_vars.ENABLE_MUTER, muter_enabled)
+    end
+end)
+
+net.Receive(network.server.SET_MUTE_DURATION, function(len, ply)
+    logInfo("Trying to set " .. con_vars.MUTE_DURATION)
+
+    if isSuperAdmin(ply) then
+        local mute_duration = net.ReadInt()
+        RunConsoleCommand(con_vars.MUTE_DURATION, mute_duration)
+    end
+end)
+
+net.Receive(network.server.SET_AUTO_CONNECT, function(len, ply)
+    logInfo("Trying to set " .. con_vars.AUTO_CONNECT)
+
+    if isSuperAdmin(ply) then
+        local auto_connect = net.ReadBit()
+        RunConsoleCommand(con_vars.AUTO_CONNECT, auto_connect)
     end
 end)
